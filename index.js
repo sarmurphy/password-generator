8af24b0349f38160fd3cd11adcfb2a2cf84e08c8
Sarah Murphy <sarahelizabeth.murphy@keyin.com>
2025-01-21 18:09:22 -0330

    Edited help messages

--- index.js
+++ index.js
@@ -6,16 +6,19 @@ const arguments = process.argv.slice(2);
 const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
 const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
-const specialCharacters = "!@#$%^&*()-_=+|;:,.<>?";
 
 // Generate help message
-const helpMessage = () => {
+if (arguments.length === 0) {
     console.log("Please review the following commands to ensure proper use:\n ");
     console.log("node index.js generate <flags>");
     console.log("");
-    console.log("--help          Displays help message with instructions on how to run this program.");
-    console.log("--length        Sets the length of your password. If length is not specified, passwords default to 8 characters.");
-    console.log("--number        Adds random numbers to the password.");
-    console.log("--uppercase     Adds random uppercase letters to the password.");
-    console.log("--symbol        Adds random symbols (special characters) to the password.");
+    console.log("--help         : Displays help message with instructions on how to run this program.");
+    console.log("--length       : Sets the length of your password. If length is not specified, passwords default to 8 characters.");
+    console.log("--number       : Adds random numbers to the password.");
+    console.log("--uppercase    : Adds random uppercase letters to the password.");
 };
+
+// Makes the password 8 characters by default if nothing is entered.
+const defaultLength = 8;
+let passwordLength = defaultLength
+
